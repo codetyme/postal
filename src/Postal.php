be9ad56553728f65e585902a4650543c5333f69c
Rohit Suthar <rohisuthar@gmail.com>
2022-10-11 11:23:43 +0530

    Initial release

--- src/Postal.php
+++ src/Postal.php
@@ -6,16 +6,14 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 
 class Postal {
-    public function pin(Request $request) {
-        $pin = $request->route('pin');
-        $response = Http::get('https://api.postalpincode.in/pincode/'.$pin);
+    public function pin($search = '') {
+        $response = Http::get('https://api.postalpincode.in/pincode/'.$search);
 
         return $response;
     }
 
-    public function branch(Request $request) {
-        $branch = $request->route('branch');
-        $response = Http::get('https://api.postalpincode.in/postoffice/'.$branch);
+    public function branch($search = '') {
+        $response = Http::get('https://api.postalpincode.in/postoffice/'.$search);
 
         return $response;
     }
