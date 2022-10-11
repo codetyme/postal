840511481f366eacc6405a434850f748a71b114b
Rohit Suthar <rohisuthar@gmail.com>
2022-10-11 19:16:09 +0530

    Initial release

--- src/Postal.php
+++ src/Postal.php
@@ -7,18 +7,20 @@ use Illuminate\Http\Request;
 
 class Postal {
 
-    public function pin($search = '') {
+    public function pin($search = '')
+    {
         $response = !empty($search) ? Http::get('https://api.postalpincode.in/pincode/'.trim($search)) : json_encode([['Message' => 'No records found', 'Status' => 'Error', 'PostOffice' => null]]);
 
         return response()->json(json_decode($response, true), 200, [], JSON_PRETTY_PRINT);
     }
 
-    public function branch($search = '') {
+    public function branch($search = '')
+    {
         $response = !empty($search) ? Http::get('https://api.postalpincode.in/postoffice/'.trim($search)) : json_encode([['Message' => 'No records found', 'Status' => 'Error', 'PostOffice' => null]]);
 
         return response()->json(json_decode($response, true), 200, [], JSON_PRETTY_PRINT);
     }
-    
+
 }
 
 ?>
